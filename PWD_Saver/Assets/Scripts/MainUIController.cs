6eb71b4652fcdb40030b475850feef5f8dad6584
pallaviredcastle <pallavi@redcastle.in>
2016-04-08 23:50:18 +0530

    Added Files to the git

---
+++ PWD_Saver/Assets/Scripts/MainUIController.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+public class MainUIController : MonoBehaviour {
+	
+	[System.Serializable]
+	public class LoginWindow {
+		public InputField Email_ID_fld;
+		public InputField Password_fld;
+		public Toggle isSavedPassword_tgle;
+	}
+	[System.Serializable]
+	public class RegisterWindow {
+		public InputField Email_ID_fld;
+		public InputField Password_fld;
+		public InputField ConfirmPassword_fld;
+	}
+
+	[System.Serializable]
+	public class AppWindow
+	{
+		public string name;
+		public GameObject window;
+	}
+
+
+	public LoginWindow loginWindow;
+
+	public List<AppWindow> AppWindows;
+
+	// Use this for initialization
+	void Start () {
+		OnHomeWindowChoose ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+		
+	public void OnHomeWindowChoose () {
+		SwitchWindows ("HomeWindow");
+	}
+
+	public void OnSavePasswordWindowChoose () {
+		SwitchWindows ("PasswordWindow");
+	}
+
+	public void OnDateReminderWindowChoose () {
+		SwitchWindows ("DateReminderWindow");
+	}
+
+	private void SwitchWindows (string _winName) {
+		
+		foreach (var apw in AppWindows) {
+			if (apw.name.Equals (_winName)) {
+				apw.window.SetActive (true);
+			} else {
+				apw.window.SetActive (false);
+			}
+		}
+	}
+
+
+}
